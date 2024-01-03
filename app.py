80c8fe65b569e5faadb445338a2356affe7c51ff
jebbshshxh <107369455+jebbshshxh@users.noreply.github.com>
2024-01-04 06:26:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
-from flask import Flask
-import os
+from flask import Flask, request
+import requests
+import json
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, world!'
+def index():
+  return 'Hello from Flask!'
+
+
+@app.route('/emulate')
+def emulate():
+  try:
+    url = str(request.args.get('url'))
+    domain = str(request.args.get('domain'))
+    data = str(request.args.get('data'))
+    data = json.loads(data)
+    resp = requests.post(url, data=data)
+    return resp.text.replace('href="/',
+                             f'href="{domain}/').replace(
+                                 'src="/', f'src="{domain}/')
+  except Exception as _ex:
+    return f'{_ex}'
