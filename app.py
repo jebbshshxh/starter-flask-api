098c09869532b8134ea59e6a8e1cac124452132a
jebbshshxh <107369455+jebbshshxh@users.noreply.github.com>
2024-02-15 20:31:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import requests
 import json
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -23,3 +24,48 @@ def emulate():
                                  'src="/', f'src="{domain}/')
   except Exception as _ex:
     return f'{_ex}'
+
+@app.route('/bs/<string:tag>/stats')
+def bs_stats(tag):
+  page = requests.get(f'https://brawltime.ninja/profile/{tag}')
+  doc = BeautifulSoup(page.text, 'html.parser')
+
+  profile = doc.find(id='aside')
+
+  pic = profile.find('img')['src'].replace('200', '50')
+  name = profile.findAll('span')[0].text
+  name_color = profile.findAll('span')[0]['style'].replace('color:', '').replace(';', '')
+  try:
+    club = profile.findAll('span')[2].text
+  except:
+    club = None
+  data = profile.findAll('dd')
+  if len(data) == 9:
+    trophies = data[3].text.replace(' ', '')
+    highest = data[4].text.replace(' ', '')
+    exp = data[5].text.replace(' ', '')
+    trio = data[6].text.replace(' ', '')
+    solo = data[7].text.replace(' ', '')
+    duo = data[8].text.replace(' ', '')
+  elif len(data) == 8:
+    trophies = data[2].text.replace(' ', '')
+    highest = data[3].text.replace(' ', '')
+    exp = data[4].text.replace(' ', '')
+    trio = data[5].text.replace(' ', '')
+    solo = data[6].text.replace(' ', '')
+    duo = data[7].text.replace(' ', '')
+
+  output = {
+    	'pic': pic,
+    	'name': name,
+    	'name_color': name_color,
+    	'tag': tag,
+    	'club': club,
+    	'trophies': trophies,
+    	'highest': highest,
+    	'exp': exp,
+    	'trio': trio,
+    	'solo': solo,
+    	'duo': duo
+    }
+  return output
